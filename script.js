8fc87c9680c324ffda70749f2e9815f8102e1bc4
kajal-1999-cloud <130256390+kajal-1999-cloud@users.noreply.github.com>
2023-08-15 14:58:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 //your JS code here. If required.
+const bodyElemnt = document.body;
+// bodyElement.style.backgroundColor = 'lightblue';
+// document.body.createElement("p").inner
+bodyElement.innerHTML = "<p>DOM load success<p>"
